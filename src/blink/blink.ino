843639dcc56150bd7f0e4520af3f089053cb2b3e
Neal Fennimore <hi@neal.codes>
2021-02-17 00:26:02 -0500

    Add in intellisense esp8266 support

--- src/blink/blink.ino
+++ src/blink/blink.ino
@@ -8,10 +8,11 @@
 
   Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
 */
+#include <Arduino.h>
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
 }
 
 // the loop function runs over and over again forever
